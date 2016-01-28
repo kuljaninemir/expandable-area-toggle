4548faca6017ce1269ab0de891786945c1f7f55f
Emir Kuljanin <emir.kuljanin@gavle.se>
2016-01-28 10:30:28 +0100

    Add another data tag for open and close glyphicon option

--- example/expandableareatoggle.js
+++ example/expandableareatoggle.js
@@ -1,40 +1,46 @@
 $(function () {
 
-	var open = "open";
-	var closed = "closed";
-	var dataGkAreaState = "data-gk-area-state";
+    var open = "open";
+    var closed = "closed";
+    var dataGkAreaState = "data-gk-area-state";
     var areaState = "gkAreaState";
-	var openClass = "gkOpenClass";
-	var openText = "gkOpenText";
-	var closeText = "gkCloseText";
+    var openClass = "gkOpenClass";
+    var openText = "gkOpenText";
+    var closeText = "gkCloseText";
+    var spanOpenGlyphicon = "gkOpenSpanGlyph";
+    var spanCloseGlyphicon = "gkCloseSpanGlyph";
 
-    $('['+dataGkAreaState+']').each(function (index) {
+    $('[' + dataGkAreaState + ']').each(function (index) {
 
-    	if ($(this).data(areaState) === open)
-    	{
-    		$("." + $(this).data(openClass)).show();
-    		if ($(this).data().hasOwnProperty(closeText))
-    			$(this).text($(this).data(closeText));
-    	}
-    	else
-    	{
-    		$("." + $(this).data(openClass)).hide();
-    		if ($(this).data().hasOwnProperty(openText))
-    			$(this).text($(this).data(openText));
-    	}
+        if ($(this).data(areaState) === open) {
+            $("." + $(this).data(openClass)).show();
+            if ($(this).data().hasOwnProperty(closeText))
+                $(this).text($(this).data(closeText));
+            if ($(this).data().hasOwnProperty(spanCloseGlyphicon))
+                $(this).removeClass($(this).data(spanOpenGlyphicon)).addClass($(this).data(spanCloseGlyphicon));
+        }
+        else {
+            $("." + $(this).data(openClass)).hide();
+            if ($(this).data().hasOwnProperty(openText))
+                $(this).text($(this).data(openText));
+            if ($(this).data().hasOwnProperty(spanOpenGlyphicon))
+                $(this).removeClass($(this).data(spanCloseGlyphicon)).addClass($(this).data(spanOpenGlyphicon));
+        }
 
         $(this).bind("click", function () {
-            if ($(this).data(areaState) === open)
-            {
+            if ($(this).data(areaState) === open) {
                 $(this).data(areaState, closed);
-            	if ($(this).data().hasOwnProperty(openText))
-                	$(this).text($(this).data(openText));
+                if ($(this).data().hasOwnProperty(openText))
+                    $(this).text($(this).data(openText));
+                if ($(this).data().hasOwnProperty(spanOpenGlyphicon))
+                    $(this).removeClass($(this).data(spanCloseGlyphicon)).addClass($(this).data(spanOpenGlyphicon));
             }
-            else
-            {
+            else {
                 $(this).data(areaState, open);
-            	if ($(this).data().hasOwnProperty(closeText))
-                	$(this).text($(this).data(closeText));
+                if ($(this).data().hasOwnProperty(closeText))
+                    $(this).text($(this).data(closeText));
+                if ($(this).data().hasOwnProperty(spanCloseGlyphicon))
+                    $(this).removeClass($(this).data(spanOpenGlyphicon)).addClass($(this).data(spanCloseGlyphicon));
             }
 
             $("." + $(this).data(openClass)).slideToggle();
